4e4111693e3eaff568061fbd8443d02efe622c40
alofred1 <gtested001@gmail.com>
2026-09-11 03:37:57 +0800

    Delete js/firebase-config.js

--- js/firebase-config.js
+++
@@ -1,1 +0,0 @@
-window.FIREBASE_CONFIG={apiKey:"AIzaSyAF0QNSqQNU1IenDpSfEVjnetMAPCZV4pRQ",authDomain:"bahasa-mandarin.firebaseapp.com",databaseURL:"https://bahasa-mandarin-default-rtdb.firebaseio.com/",projectId:"bahasa-mandarin",storageBucket:"bahasa-mandarin.firebasestorage.app",messagingSenderId:"962821886407",appId:"1:962821886407:web:8605abe8b0399a5afa09e2"};
